9f4a98385a2f506425bb6ed6561a306773914203
Cristian Loera <loera_73@hotmail.com>
2024-06-09 22:19:02 -0600

    Update the apis of notes.js

--- routes/notes.js
+++ routes/notes.js
@@ -1,43 +1,71 @@
 const notes = require('express').Router();
+const fs = require('fs');
 const { v4: uuidv4 } = require('uuid');
-const { readFromFile, readAndAppend, writeToFile } = require('../helpers/fsUtils');
+const path = require('path');
+
 
 // GET Route for retrieveing all the notes
 notes.get('/', (req, res) => {
-  readFromFile('./db/db.json')
-  .then((data) => res.json(JSON.parse(data)))}
-);
+  fs.readFile(path.join(__dirname, '../db/db.json'), 'utf-8', (err, data) => {
+    if (err) {
+      console.error(err)
+      return res.status(400).json({ error: 'Failed to read the notes' });
+    } else {
+      // Convert to string into JSON object 
+      const parseNote = JSON.parse(data);
+      res.status(201).json(parseNote);
+    };
+  }
+)});
 
 // GET Route for a specific tip
-notes.get('/:note_id', (req, res) => {
-  const noteId = req.params.note_id;
-  readFromFile('./db/db.json')
-    .then((data) => JSON.parse(data))
-    .then((json) => {
-      const result = json.filter((note) => note.note_id === noteId);
-      return result.length > 0
-        ? res.json(result)
-        : res.json('No note with that ID');
-    });
+notes.get('/:id', (req, res) => {
+  const noteId = req.params.id;
+  console.log(noteId);
+  fs.readFile(path.join(__dirname, '../db/db.json'), 'utf-8', (err, data) => {
+    if (err) {
+      console.error(err)
+      return res.status(400).json({ error: "Can't read file" });
+    } else {
+      // Convert to String into JSON object
+      let note = JSON.parse(data);
+      // find the note by it's id
+      note = note.find((note) => note.id === noteId);
+      
+      if (!note) {
+        return res.status(400).json({ error: 'Note not found' });
+      } else {
+        res.status(201).json(note);
+      }  
+    }});
 });
 
 // DELETE Route for a specific note
-notes.delete('/:note_id', (req, res) => {
-  const noteId = req.params.note_id;
-  readFromFile('./db/db.json')
-    .then((data) => JSON.parse(data))
-    .then((json) => {
-      // Make a new array of all tips except the one with the ID provided in the URL
-      const result = json.filter((note) => note.note_id !== noteId);
+notes.delete('/:id', (req, res) => {
+  const noteId = req.params.id;
+  fs.readFile(path.join(__dirname, '../db/db.json'), 'utf-8', (err, data) => {
+    if (err) {
+      console.error(err)
+      return res.status(400).json({ error: 'Failed to read the notes' });
+    } else {
+      // Convert to string into JSON objects
+      let notes = JSON.parse(data);
+      // Creates new notes without the selected note to delete
+      notes = notes.filter((note) => note.id !== noteId);
+      // Update notes
+      fs.writeFile(path.join(__dirname, '../db/db.json'), JSON.stringify(notes, null, 2), (writeErr) => {
+        if (writeErr){
+          console.log(writeErr)
+        } else {
+          console.info('Successfully updated notes')
+          res.status(200).json(notes)
+        }
+    
+        });
+    };
+})});
 
-      // Save that array to the filesystem
-      writeToFile('./db/db.json', result);
-
-      // Respond to the DELETE request
-      res.json(`Item ${noteId} has been deleted 🗑️`);
-    });
-});
-
+// POST route for a new note
 notes.post('/', (req, res) =>{
   // Destructuring the note in req.body
   const { title, text } = req.body;
@@ -46,11 +74,26 @@ notes.post('/', (req, res) =>{
     const newNote = {
       title, 
       text,
-      note_id: uuidv4()
+      id: uuidv4()
     };
     
-    readAndAppend(newNote, './db/db.json')
-    
+    // Read the file  to get the notes
+    fs.readFile(path.join(__dirname, '../db/db.json'), 'utf-8', (err, data) => {
+      if (err) {
+        console.error(err)
+      } else {
+        const notes = JSON.parse(data);
+        // Push the new note in the notes
+        notes.push(newNote);
+        //Update notes
+        fs.writeFile(path.join(__dirname, '../db/db.json'), JSON.stringify(notes, null, 2), (writeErr) => {
+          writeErr
+            ? console.log(writeErr)
+            : console.info('Successfully updated notes')
+        });
+      }
+    }
+  )
     const response = {
       status: 'success',
       body: newNote,
